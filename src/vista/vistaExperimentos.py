7c7f994428edb5580cd5f7517d942f7ee1bece99
DavidR <drprieto78@gmail.com>
2024-05-21 11:19:00 +0200

    Añadir botones expotarPDF, visualizar, cargarConfiguraciones

--- src/vista/vistaExperimentos.py
+++ src/vista/vistaExperimentos.py
@@ -22,6 +22,7 @@ class ExperimentosWindow(QWidget):
         self.main_layout.addLayout(self.crear_scroll_area(), 0, 0, 4, 6)
         self.main_layout.addWidget(self.crear_filtros_tipo_experimento(), 4, 0, 1, 2)
         self.main_layout.addWidget(self.crear_filtros_fechas(), 4, 3, 1, 3)
+        self.main_layout.addLayout(self.crear_zona_acciones(), 5, 0, 1, 6)
         self.main_layout.addWidget(btn_volver, 7, 5, 1, 1)
         
         self.setLayout(self.main_layout)
@@ -54,26 +55,39 @@ class ExperimentosWindow(QWidget):
     def crear_filtros_fechas(self):
         layout = QGridLayout()
         gb_filtrado_fecha = QGroupBox("Filtrar por fecha")
-        
-        btn_desde = QPushButton("Desde")
-        btn_hasta = QPushButton("Hasta")
+        self.calendario = QCalendarWidget()
 
-        le_desde = QLineEdit()
-        le_desde.setPlaceholderText("dd/mm/aaaa")
-        le_desde.setStyleSheet("background-color: #f0f0f0")
-        le_desde.setAlignment(Qt.AlignmentFlag.AlignCenter)
-       
-        le_hasta = QLineEdit()
-        le_hasta.setPlaceholderText("dd/mm/aaaa")
-        le_hasta.setStyleSheet("background-color: #f0f0f0")
-        le_hasta.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.btn_desde = QPushButton("Desde")
+        self.btn_hasta = QPushButton("Hasta")
+
+        self.le_desde = QLineEdit()
+        self.le_desde.setPlaceholderText("dd/mm/aaaa")
+        self.le_desde.setStyleSheet("background-color: #f0f0f0")
+        self.le_desde.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.le_desde.setDisabled(True)
+
+        self.le_hasta = QLineEdit()
+        self.le_hasta.setPlaceholderText("dd/mm/aaaa")
+        self.le_hasta.setStyleSheet("background-color: #f0f0f0")
+        self.le_hasta.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.le_hasta.setDisabled(True)
+
+
+
+        self.btn_desde.clicked.connect(self.control_fechas)
+        self.btn_desde.clicked.connect(self.calendario.hide)
+
+        self.btn_hasta.clicked.connect(self.control_fechas)
+        self.btn_hasta.clicked.connect(self.calendario.hide)
         
         
 
-        layout.addWidget(btn_desde, 0, 0)
-        layout.addWidget(btn_hasta, 1, 0)
-        layout.addWidget(le_desde, 0, 1, 1, 2)
-        layout.addWidget(le_hasta, 1, 1, 1 ,2)
+
+
+        layout.addWidget(self.btn_desde, 0, 0)
+        layout.addWidget(self.btn_hasta, 1, 0)
+        layout.addWidget(self.le_desde, 0, 1, 1, 2)
+        layout.addWidget(self.le_hasta, 1, 1, 1 ,2)
         
         gb_filtrado_fecha.setLayout(layout)
         return gb_filtrado_fecha
@@ -82,13 +96,32 @@ class ExperimentosWindow(QWidget):
 
 
     def crear_zona_acciones(self):
-        gb_acciones = QGroupBox("Acciones")
-
+        layout = QGridLayout()
+    
         btn_exportar = QPushButton("Exportar a PDF")
         btn_visualizar = QPushButton("Visualizar experimento")
         btn_configuraciones = QPushButton("Cargar configuraciones")
-        pass
+        
+        layout.addWidget(btn_exportar, 0, 0, 1, 2)    
+        layout.addWidget(btn_visualizar, 0, 2, 1, 2)
+        layout.addWidget(btn_configuraciones, 0, 4, 1, 2)
+        return layout
 
+
+    def control_fechas(self):
+        self.calendario.setGridVisible(True)
+        self.calendario.setGeometry(100, 100, 200, 200)
+        self.calendario.clicked.connect(self.mostrar_fecha)
+        #self.calendario.clicked.connect(self.calendario.hide)
+        return self.calendario
+    
+    def mostrar_fecha(self, fecha):
+        self.le_desde.setText(fecha.toString("dd/MM/yyyy"))
+        self.le_hasta.setText(fecha.toString("dd/MM/yyyy"))
+        return fecha.toString("dd/MM/yyyy")
+    
+    
+    
 
 
 
