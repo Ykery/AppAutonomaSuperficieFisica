bb60c38fc645e60f1e90343a05f4424cf600917f
Yker <ykerykery@gmail.com>
2024-06-06 10:45:33 +0200

    Crear metodo post en flask

--- flaskAutonoma/main.py
+++ flaskAutonoma/main.py
@@ -3,6 +3,7 @@ from clases import *
 from dao import *
 from flask import request
 from flask import abort
+import json
 
 
 # Inicializamos la base de datos
@@ -10,12 +11,14 @@ Conexion.iniciar_bbdd()
 # creamos el servidor flask
 app = Flask(__name__)
 
+
 @app.route("/")
 def inicio():
     return "Aplicación de gestión de experimentos científicos en Flask"
 
-#funcion encargada de dar una respuesta a la peticcion GET con esta url /experimentos  
-@app.route("/experimentos",methods=["GET"])
+
+# funcion encargada de dar una respuesta a la peticcion GET con esta url /experimentos
+@app.route("/experimentos", methods=["GET"])
 def experimentos():
     todos_experimentos = ExperimentoDAO.obtener_todos()
     # Array
@@ -27,29 +30,60 @@ def experimentos():
                 "tipo": experimento.tipo,
                 "descripcion": experimento.descripcion,
                 "experimento.descripcion": experimento.descripcion,
-                "rutaCsv": experimento.rutaCsv
+                "rutaCsv": experimento.rutaCsv,
             }
         )
     final = {}
     final["experimentos"] = datos_todos
     return final
 
-@app.route("/experimentos/<int:id>",methods=["DELETE"])
+
+@app.route("/experimentos/<int:id>", methods=["DELETE"])
 def experimentos_del(id):
     print(request)
-    if request.remote_addr != '127.0.0.1':
+    if request.remote_addr != "127.0.0.1":
         abort(403)  # Forbidden
 
     experimento = ExperimentoDAO.obtener_por_id(id)
     if experimento is None:
         abort(404)
+
     ExperimentoDAO.eliminar(experimento)
     return {
-                "id": experimento.id,
-                "tipo": experimento.tipo,
-                "descripcion": experimento.descripcion,
-                "experimento.descripcion": experimento.descripcion,
-                "rutaCsv": experimento.rutaCsv
-            }
+        "id": experimento.id,
+        "tipo": experimento.tipo,
+        "descripcion": experimento.descripcion,
+        "experimento.descripcion": experimento.descripcion,
+        "rutaCsv": experimento.rutaCsv,
+    }
 
+
+@app.route("/experimentos", methods=["POST"])
+def experimentos_post(): 
+    if request.remote_addr != "127.0.0.1":
+        abort(403)  # Forbidden
+
+
+    byte_data = request.data
+    str_data = byte_data.decode('utf-8')
+    json_data = json.loads(str_data)
     
+    experimento = Experimento()
+    experimento.tipo = json_data["tipo"]
+    experimento.descripcion = json_data["descripcion"]
+    experimento.rutaCsv = json_data["rutaCsv"]
+
+    experimento = ExperimentoDAO.crear(experimento)
+
+    print(json_data)
+    return {
+        "id": experimento.id,
+        "tipo": experimento.tipo,
+        "descripcion": experimento.descripcion,
+        "experimento.descripcion": experimento.descripcion,
+        "rutaCsv": experimento.rutaCsv,
+    }
+
+@app.route("/experimentos", methods=["PUT"])
+def experimentos_put(): 
+    ...
